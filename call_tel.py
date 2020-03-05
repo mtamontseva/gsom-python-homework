c6f4f1ef03495820bfff7b3e039c04bf2809c1ca
Maria Tamontseva <61382401+mtamontseva@users.noreply.github.com>
2020-03-05 22:02:34 +0300

    Create call_tel.py

---
+++ call_tel.py
@@ -0,0 +1,11 @@
+def price(kode, length):
+    if kode == 343:
+        return(length*15)
+    elif kode == 381:
+        return(length*18)
+    elif kode == 473:
+        return(length*13)
+    elif kode == 485:
+        return(length*11)
+    else:
+        return('Ничего не выйдет')
