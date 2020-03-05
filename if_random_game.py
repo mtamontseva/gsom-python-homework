66eff74919ccdc8d177f05df1557d590de677b23
Maria Tamontseva <61382401+mtamontseva@users.noreply.github.com>
2020-03-05 22:04:29 +0300

    Create if_random_game.py

---
+++ if_random_game.py
@@ -0,0 +1,8 @@
+chislo = int(input('Загадайте число от 1 до 4'))
+comp = random.randint(1,4)
+if chislo == comp:
+    print('Победа!')
+elif chislo < comp:
+    print('Ваше число меньше!')
+else:
+    print('Ваше число больше!')
